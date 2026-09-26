747af187d576b370969c36e6b6a13e3b2b440686
ikilledkanishk <kanishksingh450@gmail.com>
2026-09-26 21:12:54 +0530

    Reapply "Q6 to Q8"
    This reverts commit da44ba4695055849ae3fe50d48a04fc4bcd18be4.

--- Data Structrue and Algorithms (using C)/Q7.c
+++ Data Structrue and Algorithms (using C)/Q7.c
@@ -1,1 +1,150 @@
+/*Problem Statement: Write a menu driven c program to implement the following searching algorithms
+a. Linear search
+b. Binary search
+c. Interpolation search*/
 
+//Source Code:
+#include <stdio.h>
+
+#define MAX 100
+
+// Linear Search
+int linearSearch(int arr[], int n, int key)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+            return i;
+    }
+
+    return -1;
+}
+
+// Binary Search
+int binarySearch(int arr[], int n, int key)
+{
+    int low = 0, high = n - 1, mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+// Interpolation Search
+int interpolationSearch(int arr[], int n, int key)
+{
+    int low = 0, high = n - 1, pos;
+
+    while (low <= high && key >= arr[low] && key <= arr[high])
+    {
+        if (arr[low] == arr[high])
+        {
+            if (arr[low] == key)
+                return low;
+            else
+                return -1;
+        }
+
+        pos = low + ((key - arr[low]) * (high - low))
+                    / (arr[high] - arr[low]);
+
+        if (arr[pos] == key)
+            return pos;
+
+        else if (arr[pos] < key)
+            low = pos + 1;
+
+        else
+            high = pos - 1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[MAX], n, key, choice, result;
+    int i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements in ascending order:\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    do
+    {
+        printf("\n--- SEARCH MENU ---\n");
+        printf("1. Linear Search\n");
+        printf("2. Binary Search\n");
+        printf("3. Interpolation Search\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice >= 1 && choice <= 3)
+        {
+            printf("Enter the element to search: ");
+            scanf("%d", &key);
+        }
+
+        switch (choice)
+        {
+            case 1:
+                result = linearSearch(arr, n, key);
+
+                if (result != -1)
+                    printf("Element found at position %d.\n", result + 1);
+                else
+                    printf("Element not found.\n");
+
+                break;
+
+            case 2:
+                result = binarySearch(arr, n, key);
+
+                if (result != -1)
+                    printf("Element found at position %d.\n", result + 1);
+                else
+                    printf("Element not found.\n");
+
+                break;
+
+            case 3:
+                result = interpolationSearch(arr, n, key);
+
+                if (result != -1)
+                    printf("Element found at position %d.\n", result + 1);
+                else
+                    printf("Element not found.\n");
+
+                break;
+
+            case 4:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
--- Data Structrue and Algorithms (using C)/Q8.c
+++ Data Structrue and Algorithms (using C)/Q8.c
@@ -1,1 +1,151 @@
+/*Problem Statement: Write a menu driven c program to implement the following sorting algorithms
+a. Bubble sort
+b. Insertion sort
+c. Selection sort*/
 
+//Source Code:
+#include <stdio.h>
+
+#define MAX 100
+
+// Display array
+void display(int arr[], int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+// Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Insertion Sort
+void insertionSort(int arr[], int n)
+{
+    int i, j, key;
+
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+// Selection Sort
+void selectionSort(int arr[], int n)
+{
+    int i, j, min, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+int main()
+{
+    int arr[MAX], temp[MAX];
+    int n, choice, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    do
+    {
+        printf("\n--- SORTING MENU ---\n");
+        printf("1. Bubble Sort\n");
+        printf("2. Insertion Sort\n");
+        printf("3. Selection Sort\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        // Copy original array
+        for (i = 0; i < n; i++)
+        {
+            temp[i] = arr[i];
+        }
+
+        switch (choice)
+        {
+            case 1:
+                bubbleSort(temp, n);
+
+                printf("Array after Bubble Sort:\n");
+                display(temp, n);
+                break;
+
+            case 2:
+                insertionSort(temp, n);
+
+                printf("Array after Insertion Sort:\n");
+                display(temp, n);
+                break;
+
+            case 3:
+                selectionSort(temp, n);
+
+                printf("Array after Selection Sort:\n");
+                display(temp, n);
+                break;
+
+            case 4:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
